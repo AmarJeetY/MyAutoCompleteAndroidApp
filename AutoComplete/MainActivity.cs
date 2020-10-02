a578b5b0fc2aca3c0a2ead5235acacaad4cee763
AmarjeetY <yelwande@gmail.com>
2020-10-02 14:52:27 +0100

    minor refactoring

--- AutoComplete/MainActivity.cs
+++ AutoComplete/MainActivity.cs
@@ -13,7 +13,6 @@ namespace AutoComplete
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-       
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -21,43 +20,31 @@ namespace AutoComplete
             
             SetContentView(Resource.Layout.activity_main);
             
-            ArrayAdapter adapter =  new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerItem, GetAllWords());
-            var autoComplete1 = FindViewById<AutoCompleteTextView>(Resource.Id.autoCompletionWord);
+            var adapter =  new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerItem, GetAllWords());
+            var autoComplete = FindViewById<AutoCompleteTextView>(Resource.Id.autoCompletionWord);
             
-            autoComplete1.Adapter = adapter;
+            autoComplete.Adapter = adapter;
         }
 
         private List<string> GetAllWords()
         {
             var rgx = new Regex("^[a-zA-Z]+$");
             var appDirectory = Application.Context.GetExternalFilesDir(null).AbsolutePath;
-            var textFilePath = Path.Combine(appDirectory, "DataFile.csv");
-            var file = File.CreateText(textFilePath);
+            var csvFilePath = Path.Combine(appDirectory, "DataFile.csv");
+            var file = File.CreateText(csvFilePath);
             file.Write("hello, helium, height, high, horse, flower, florence");
             file.Close();
-            var text123 = File.ReadAllLines(textFilePath);
-            var results = File.ReadAllLines(textFilePath)
+            
+            var allWords = File.ReadAllLines(csvFilePath)
                 .SelectMany(line => line.Split(','))
                 .SelectMany(line => line.Split(' '))
-
-                //.GroupBy(word => word)
                 .ToList();
-                //.RemoveAll(m => criteria.Any(crit => m.rgx.Contains(crit)););
 
-
-            var list = new List<string>();
-            foreach (var x in results)
-            {
-                if (rgx.IsMatch(x.ToString())) list.Add(x.ToString());
-            }
-
-            return list;
-
+            return allWords.Where(word => rgx.IsMatch(word)).ToList();
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
